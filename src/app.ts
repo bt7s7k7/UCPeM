f1f7368d168da9ea754c05a6a19cc2c1cb66f041
bt7s7k7 <bt7s7k7@hotmail.sk>
2020-04-22 11:34:37 +0200

    Config parsing

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,44 @@
-console.log("Hello world")
+import { UserError } from "./UserError"
+import { getFolderInfo } from "./project"
+import { inspect } from "util"
+
+const args = process.argv.slice(2)
+
+var commads = {
+    install: {
+        desc: "Downloads and prepares all ports",
+        async callback() {
+
+        }
+    },
+    prepare: {
+        desc: "Runs the prepare script for this package",
+        async callback() {
+
+        }
+    },
+    info: {
+        desc: "Prints imports and config files of the current project",
+        async callback() {
+            let info = await getFolderInfo(".")
+            console.log(inspect(info, { colors: true, depth: Infinity }))
+        }
+    }
+} as Record<string, { desc: string, callback: () => Promise<void> }>
+
+if (args.length == 0 || !(args[0] in commads)) {
+    let commandDefs = Object.entries(commads)
+    let maxNameLength = commandDefs.reduce((p, v) => Math.max(p, v[0].length), 0) + 1
+    console.log("Usage:\n  ucpem <operation>\n\nOperations:")
+    commandDefs.forEach(v => {
+        console.log(`  ${v[0]}${" ".repeat(maxNameLength - v[0].length)}- ${v[1].desc}`)
+    })
+} else {
+    commads[args[0]].callback().catch(err => {
+        if (err instanceof UserError) {
+            console.error(`[ERR] ${err.message}`)
+        } else {
+            console.error(err)
+        }
+    })
+}
---
+++ src/project.ts
@@ -0,0 +1,98 @@
+import * as path from "path"
+import { promisify } from "util"
+import { readFile, stat } from "fs"
+import { UserError } from "./UserError"
+
+export interface IResource {
+    name: string, 
+}
+
+export interface IImportDeclaration {
+    port: string,
+    resources: IResource[]
+}
+
+export function parseConfigFile(content: string, folder: string) {
+    const lines = content.split(/\n/g)
+    let prepare = [] as string[]
+    let ports = {} as Record<string, IImportDeclaration>
+    let exports = [] as IResource[]
+
+    var state = "normal" as "normal" | "import" | "prepare" | "export"
+    var lastImport = null as IImportDeclaration | null
+
+    for (let i = 0, len = lines.length; i < len; i++) {
+        const getPos = () => `${folder}:${i + 1}`
+        let line = lines[i].trim()
+        let words = line.split(/\s/g).filter(v=>v.length > 0)
+        if (line[0] == "#" || line.length == 0) continue
+        if (state == "normal") {
+            if (line == "prepare") {
+                state = "prepare"
+            } else if (words[0] == "import") {
+                if (words.length == 2) {
+                    const portName = words[1]
+                    if (!(portName in ports)) {
+                        ports[portName] = {
+                            port: portName,
+                            resources: []
+                        }
+                    }
+                    
+                    lastImport = ports[portName]
+                    state = "import"
+                } else {
+                    throw new UserError(`Import keywork should have 1 argument ("import" <port>) but ${words.length - 1} found at ${getPos()}`)
+                }
+            } else if (line == "export") {
+                state = "export"
+            } else throw new UserError(`Invalid keyword "${line}" at ${getPos()}`)
+        } else if (state == "prepare") {
+            if (line == "end") {
+                state = "normal"
+            } else {
+                prepare.push(line)
+            }
+        } else if (state == "import") {
+            if (line == "end") {
+                state = "normal"
+            } else {
+                if (words.length > 1) throw new UserError(`Resource name cannot contain whitespace at ${getPos()}`)
+                if (!lastImport) throw new Error("Last import not set in import state")
+                lastImport.resources.push({ name: words[0] })
+            }
+        } else if (state == "export") {
+            if (line == "end") {
+                state = "normal"
+            } else {
+                if (words.length > 1) throw new UserError(`Resource name cannot contain whitespace at ${getPos()}`)
+                exports.push({ name: words[0] })
+            }
+        }
+    }
+
+    return {
+        prepare,
+        ports,
+        exports
+    }
+}
+
+export async function getFolderInfo(folder: string) {
+    const absolutePath = path.resolve(process.cwd(), folder)
+    let configText = ""
+    try {
+        configText = (await promisify(readFile)(path.join(absolutePath, "~ucpem_config"))).toString()
+    } catch (err) {
+        if ((err as NodeJS.ErrnoException).code != "ENOENT") {
+            throw err
+        } else {
+            throw new UserError(`Failed to find config file in ${folder}`)
+        }
+    }
+
+    return {
+        path: absolutePath,
+        ...parseConfigFile(configText, absolutePath)
+    }
+}

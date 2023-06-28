5c4afa94b0abd8bd2c1e5fde947ceb8268057a0c
Branislav Trstenský <bt7s7k7@hotmail.sk>
2023-06-28 15:23:06 +0200

    Printing global script names when logging script resource

--- src/Project/Resource.ts
+++ src/Project/Resource.ts
@@ -1,19 +1,22 @@
 import chalk from "chalk"
 import { statSync } from "fs"
 import { basename, join } from "path"
-import { SCRIPT_RES_PREFIX } from "../global"
 import { GitIgnoreGenerator } from "../Install/GitIgnoreGenerator"
+import { SCRIPT_RES_PREFIX } from "../global"
 import { DependencyTracker } from "./DependencyTracker"
-import { link } from "./link"
 import { PrepareScript } from "./PrepareScript"
 import { Project } from "./Project"
+import { link } from "./link"
 import { parseResourceID } from "./util"
 
 export class Resource {
-    public readonly isScript = parseResourceID(this.id).resourceName.startsWith(SCRIPT_RES_PREFIX)
+    public readonly resourceName: string
+    public readonly portName: string
+    public readonly isScript: boolean
+    public readonly scriptName: string | null
 
     logTree(prefix = "", postPrefix = "", shallow: boolean | "shallow" = false) {
-        console.log(prefix + this.id + (this.internal ? " !!INT" : ""))
+        console.log(prefix + this.id + (this.scriptName != null ? ` @/${this.portName}+${this.scriptName}` : "") + (this.internal ? " !!INT" : ""))
         if (!shallow) this.dependencies.forEach((dependency, index, { length }) => {
             const depResource = DependencyTracker.resolveResource(dependency)
             const last = index == length - 1
@@ -57,6 +60,16 @@ export class Resource {
         public readonly prepare: PrepareScript | null,
         public readonly internal: boolean,
     ) {
+        const { portName, resourceName } = parseResourceID(this.id)
+        this.portName = portName
+        this.isScript = resourceName.startsWith(SCRIPT_RES_PREFIX)
+        this.resourceName = resourceName
+        if (this.isScript) {
+            this.scriptName = resourceName.slice(SCRIPT_RES_PREFIX.length)
+        } else {
+            this.scriptName = null
+        }
+
         if (!internal && !this.isScript) {
             try {
                 if (!statSync(path).isDirectory()) {
